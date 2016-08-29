72a74d2d8a693c092c59c0c62216951892dee751
Melina Vidoni <melinavidoni@gmail.com>
2016-08-29 12:08:02 -0300

    Textfield works with enter key, and cleans when writing. Part of Issue #3.

--- src/gui/controller/MainPanelController.java
+++ src/gui/controller/MainPanelController.java
@@ -2,12 +2,11 @@ package gui.controller;
 
 
 import bot.agent.ChatbotSimulator;
+import bot.knowledge.auxialiary.WordNormalizer;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-
+import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 
 /**
@@ -16,17 +15,21 @@ import javafx.scene.control.TextField;
  * @author Melina Vidoni, INGAR CONICET-UTN, 2016.
  */
 public class MainPanelController extends Controller {
-    @FXML
-    private Button askButton;
+    @FXML private TextField askTextField;
+    @FXML private Label answerLabel;
 
-    @FXML
-    private TextField askTextField;
+    @FXML private Label suggestionsLabel;
 
-    @FXML
-    private TextArea answerTextArea;
+    @FXML private Menu languagesMenu;
+    @FXML private RadioMenuItem englishMenuItem;
+    @FXML private RadioMenuItem spanishMenuItem;
+    @FXML private Menu helpMenu;
+    @FXML private MenuItem creditsMenu;
 
-    @FXML
-    private Label suggestionsLabel;
+    /**
+     * Flag for the key listener to clean the text field.
+     */
+    private boolean firstKey;
 
     /**
      * A reference to the simulator of the agent.
@@ -39,26 +42,68 @@ public class MainPanelController extends Controller {
      * after the fxml file has been loaded.
      */
     @FXML
-    private void initialize() { }
+    private void initialize() {
+        // Focus on the text field
+        askTextField.requestFocus();
 
-
-
-    @FXML
-    public void askQuestion() {
-        // The simulator reads the question
-        simulator.setReadStatement(askTextField.getText());
-
-        // Start the simulator
-        simulator.start();
-
-        // Print the answer
-        String answer = simulator.getFinalAnswer();
-
-        System.out.println("Answer on controller: " + answer );
-
-        // Show the answer
-        answerTextArea.setText( answer );
+        // Set the flag as true;
+        firstKey = true;
     }
+
+
+    /**
+     * Listener for the label. The user must press enter to launch the evaluation.
+     * @param keyEvent They event to evaluate.
+     */
+    @FXML
+    public void askEnterPressed(KeyEvent keyEvent) {
+        // If the enter key was pressed
+        if(keyEvent.getCode().equals(KeyCode.ENTER)) {
+            // Call the private method
+            getAnAswer();
+
+            // Restart the flag
+            firstKey = true;
+        }
+        // If not, if the user is writting and this is the first key
+        else if(firstKey){
+            // Set the flag as false
+            firstKey = false;
+            // And clear the text field
+            askTextField.setText("");
+        }
+    }
+
+
+    /**
+     * Method that launches the simulation and attempts to find an answer
+     * for the question the user entered. It checks that the text field is
+     * not empty, or it shows an error.
+     */
+    private void getAnAswer() {
+        // If the textfield is not empty
+        if(!askTextField.getText().isEmpty()
+                && !WordNormalizer.normalizeWord(askTextField.getText()).isEmpty()) {
+            // The simulator reads the question
+            simulator.setReadStatement(askTextField.getText());
+
+            // Start the simulator
+            simulator.start();
+
+            // Print the answer
+            String answer = simulator.getFinalAnswer();
+
+            System.out.println("Answer on controller: " + answer );
+
+            // Show the answer
+            answerLabel.setText( answer );
+        }
+        else {
+            // TODO CHANGE BORDER
+        }
+    }
+
+
 
 
     /**
